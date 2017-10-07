f866fd6375634e6749139af7084c71169760aa11
Shane Moodie <shanepmoodie96@gmail.com>
2017-10-07 13:20:12 -0400

    Second commit

--- Lucky Numbers/Program.cs
+++ Lucky Numbers/Program.cs
@@ -10,6 +10,28 @@ namespace Lucky_Numbers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter the lowest digit of your range.");
+            int lowDigit = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Now please enter the highest digit of your range.");
+            int highDigit = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Next, guess six lucky numbers from your range!");
+            int[] luckyNumbers = new int[6];
+
+            for (int i = 0; i < luckyNumbers.Length; i++)
+            {
+                luckyNumbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            if (luckyNumbers > lowDigit | luckyNumbers < highDigit)
+            {
+                Console.WriteLine("Please enter a valid number from within your range.");
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
         }
     }
 }
